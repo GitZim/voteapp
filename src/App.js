87d43fab08902305a08290d916978a3c2a7dadb1
Ovey <ovey@pop-os.localdomain>
2023-03-25 22:53:30 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import web3 from "./web3";
+import lottery, { contractAddress, nachosToken } from "./lottery";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      owner: "",
+      currentProposal: {},
+      value: "",
+      accounts: [],
+      tokenAmount: "",
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const owner = await lottery.methods.owner().call();
+      const proposalId = 1; // Set this to the desired proposal ID
+      const currentProposal = await lottery.methods
+        .getProposal(proposalId)
+        .call();
+      const accounts = await web3.eth.getAccounts();
+
+      // Update the state with the fetched data
+      this.setState({
+        owner,
+        accounts,
+        currentProposal: {
+          title: currentProposal[0],
+          description: currentProposal[1],
+          yesVotes: currentProposal[2],
+          noVotes: currentProposal[3],
+        },
+      });
+    } catch (error) {
+      console.error("Error fetching data from the contract:", error);
+    }
+  }
+
+  onVoteYes = async () => {
+    const { accounts, tokenAmount } = this.state;
+    const nachosTokenAmount = web3.utils.toWei(tokenAmount, "ether");
+
+    await nachosToken.methods
+      .approve(contractAddress, nachosTokenAmount)
+      .send({ from: accounts[0] });
+
+    await lottery.methods.vote(1, true).send({ from: accounts[0] });
+  };
+
+  onVoteNo = async () => {
+    const { accounts, tokenAmount } = this.state;
+    const nachosTokenAmount = web3.utils.toWei(tokenAmount, "ether");
+
+    await nachosToken.methods
+      .approve(contractAddress, nachosTokenAmount)
+      .send({ from: accounts[0] });
+
+    await lottery.methods.vote(1, false).send({ from: accounts[0] });
+  };
+
+  render() {
+    const { currentProposal } = this.state;
+
+    console.log("Running web3 " + web3.version);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React App Loaded{" "}
+          </a>
+          <h2>Voting Contract</h2>
+          <p>This contract is owned by {this.state.owner}</p>
+          <p>Contract address: {contractAddress}</p>
+          <h3>Current Proposal</h3>
+          <p>Title: {currentProposal.title}</p>
+          <p>Description: {currentProposal.description}</p>
+          <p>Yes Votes: {currentProposal.yesVotes}</p>
+          <p>No Votes: {currentProposal.noVotes}</p>
+          <div className="vote-buttons">
+            <input
+              type="number"
+              placeholder="Token amount"
+              onChange={(event) =>
+                this.setState({ tokenAmount: event.target.value })
+              }
+            />
+            <button onClick={this.onVoteYes} className="yes">
+              Vote Yes
+            </button>
+            <button onClick={this.onVoteNo}>Vote No</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/lottery.js
@@ -0,0 +1,256 @@
+import web3 from "./web3";
+
+const address = "0x5e47b441C4f3a80E5C9160bE7FbE4368728Df1a3";
+
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: "contract IERC20",
+        name: "_nachosToken",
+        type: "address",
+      },
+      { internalType: "uint256", name: "_voteLimit", type: "uint256" },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      { internalType: "string", name: "_title", type: "string" },
+      { internalType: "string", name: "_description", type: "string" },
+      { internalType: "uint256", name: "_voteLimit", type: "uint256" },
+    ],
+    name: "createProposal",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "_proposalId", type: "uint256" }],
+    name: "executeProposal",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "_proposalId", type: "uint256" }],
+    name: "getProposal",
+    outputs: [
+      { internalType: "string", name: "", type: "string" },
+      { internalType: "string", name: "", type: "string" },
+      { internalType: "uint256", name: "", type: "uint256" },
+      { internalType: "uint256", name: "", type: "uint256" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "nachosToken",
+    outputs: [{ internalType: "contract IERC20", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "proposalCount",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "proposals",
+    outputs: [
+      { internalType: "string", name: "title", type: "string" },
+      { internalType: "string", name: "description", type: "string" },
+      { internalType: "uint256", name: "yesVotes", type: "uint256" },
+      { internalType: "uint256", name: "noVotes", type: "uint256" },
+      { internalType: "bool", name: "proposalPassed", type: "bool" },
+      { internalType: "bool", name: "proposalExecuted", type: "bool" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "_proposalId", type: "uint256" },
+      { internalType: "bool", name: "_vote", type: "bool" },
+    ],
+    name: "vote",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "voteLimit",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "_recipient", type: "address" },
+      { internalType: "uint256", name: "_amount", type: "uint256" },
+    ],
+    name: "withdrawTokens",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+const nachosTokenAddress = "0x751d34F0e107b5aBD30D308E30117d403d517eE6";
+
+const nachosTokenAbi = [
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "from", type: "address" },
+      { indexed: true, internalType: "address", name: "to", type: "address" },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "_owner", type: "address" },
+      { internalType: "address", name: "_spender", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
+    ],
+    name: "allowed",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "_spender", type: "address" },
+      { internalType: "uint256", name: "_value", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "_owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "balances",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "_to", type: "address" },
+      { internalType: "uint256", name: "_value", type: "uint256" },
+    ],
+    name: "transfer",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "_from", type: "address" },
+      { internalType: "address", name: "_to", type: "address" },
+      { internalType: "uint256", name: "_value", type: "uint256" },
+    ],
+    name: "transferFrom",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+export const contractAddress = "0x5e47b441C4f3a80E5C9160bE7FbE4368728Df1a3";
+
+export default new web3.eth.Contract(abi, address);
+const nachosToken = new web3.eth.Contract(nachosTokenAbi, nachosTokenAddress);
+
+export { nachosToken };
